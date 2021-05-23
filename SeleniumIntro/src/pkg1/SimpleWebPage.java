f962f9117e8721f6f480b4030815c67533d0a4c0
deepakchanana <deepakdeep1992@gmail.com>
2021-05-22 21:47:50 -0700

    single file changes

--- SeleniumIntro/src/pkg1/SimpleWebPage.java
+++ SeleniumIntro/src/pkg1/SimpleWebPage.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SimpleWebPage
 {
 public static void main(String[] args) {
+	System.out.println("Changes done by vidhi");
 	
 	for(int i=1;i<=5;i++)  // loop for rows i---2
 	{
